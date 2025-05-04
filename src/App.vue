2c211a331eb714669965641748c4f9f8b44399fc
Chris <c.lavender@pm.me>
2025-05-04 15:59:26 +0100

    Initial vue site commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Navbar from './components/ComponentNavbar.vue';
 </script>
 
 <style>
---
+++ src/views/PageHome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Welcome to My Vue Website!</h1>
+      <p>This is a basic homepage built with Vue.js.</p>
+    </div>
+  </template>
