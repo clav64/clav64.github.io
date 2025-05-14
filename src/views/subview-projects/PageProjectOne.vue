199c82d79f09a4516a12467acb5831c4c53ce04e
Chris <c.lavender@pm.me>
2025-05-14 22:59:50 +0100

    update

--- src/views/subview-projects/PageProjectOne.vue
+++ src/views/subview-projects/PageProjectOne.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
       <h1>Project One</h1>
-        <p>This is the first project. It showcases my skills in Vue.js and web development. Example.</p>
+        <p>This is the first project. It showcases my skills in Vue.js and web development.</p>
     </div>
   </template>
